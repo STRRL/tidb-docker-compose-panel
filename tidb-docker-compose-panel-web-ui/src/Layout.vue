379d85a450668118c82e1be5e8f76984044c8491
STRRL <str_ruiling@outlook.com>
2019-08-29 02:04:25 +0800

    :sparkles: Add button: back to home

--- tidb-docker-compose-panel-web-ui/src/Layout.vue
+++ tidb-docker-compose-panel-web-ui/src/Layout.vue
@@ -4,6 +4,10 @@
       <el-header>
         <h1>TiDB docker compose panel</h1>
       </el-header>
+      <el-button
+        type="text"
+        @click="backToHome"
+      >Back to home</el-button>
       <el-main>
         <router-view />
       </el-main>
@@ -13,7 +17,12 @@
 
 <script>
 export default {
-  name: "layout"
+  name: "layout",
+  methods: {
+    backToHome () {
+      this.$router.push({ name: "home" });
+    }
+  }
 }
 </script>
 <style>
